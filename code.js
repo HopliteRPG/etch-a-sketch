7ff2056a32cfb7723c7d62dd5c0bde56c6c50781
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2024-04-08 21:07:48 -0400

    created function that will allow me to add multiple divs to content

---
+++ code.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container")
+
+
+function cloneDiv(){
+    const div = document.createElement("div");
+    div.classList.add("divBox");
+    div.textContent = "this is a test";
+    container.appendChild(div);
+}
+
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
+cloneDiv()
