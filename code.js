58766573d739a8be76333db3be4e2e2d02f80d7c
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2024-04-08 21:25:26 -0400

    created a variable named divRow my plan is to create a new one each row and add the divs to it

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container")
+const divRow = document.createElement("div");
+
 
 function cloneDiv(){
     const div = document.createElement("div");
@@ -6,10 +8,10 @@ function cloneDiv(){
     container.appendChild(div);
 }
 
-
 for(let i = 0; i<16; i++){
     for(let i = 0; i<16;i++){
+        counter++;
         cloneDiv();
     }
-
 }
+
