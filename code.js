5a5e5ae2c9247d86de187c17da0bbad54be11d19
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2024-04-10 12:55:48 -0400

    going to try new things saving this just incase

--- code.js
+++ code.js
@@ -1,16 +1,22 @@
-const container = document.querySelector(".container")
-const divRow = document.createElement("div");
+const container = document.querySelector(".container");
 
+//Create a new divRow each time and add the 16 divs into it
+
+function cloneRow(){
+    const divRow = document.createElement("div");
+    divRow.classList.add("divRow");
+    container.appendChild(divRow)
+}
 
 function cloneDiv(){
     const div = document.createElement("div");
     div.classList.add("divBox");
-    container.appendChild(div);
+    divRow.appendChild(div);
 }
 
 for(let i = 0; i<16; i++){
+    cloneRow();
     for(let i = 0; i<16;i++){
-        counter++;
         cloneDiv();
     }
 }
